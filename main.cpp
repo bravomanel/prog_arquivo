5869963abe25b6b8e82b322825676789750e10cf
Emanuel Bravo <emanuelbravo0706@gmail.com>
2024-05-28 17:49:00 -0300

    Filtrar consulta por valor

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 #include <string>
 #include <cstdlib>
 #include <climits>
-
-using namespace std;
+#include <ctime>
 
 #define FILE_NAME_F "transacoes.csv"
 #define FILE_NAME_10 "tran10k.csv"
 #define FILE_NAME_1 "trank.csv"
 
+using namespace std;
 
 void clearScreen() {
     #ifdef _WIN32
@@ -90,44 +90,7 @@ bool stringParaFloat(string& str, float& result, int linhaAtual) {
     return true;
 }
 
-vector<Cliente> filtrarMovimentacoesPorMesAno(vector<Transacao>& transacoes, short mesDesejado, short anoDesejado) {
-    vector<Cliente> movimentacoesFiltradas;
-
-    for (const auto& t : transacoes) {
-        if (t.mes == mesDesejado && t.ano == anoDesejado) {
-            Cliente c;
-            bool existe = false;
-            for (auto& m : movimentacoesFiltradas) {
-                if (m.agencia == t.agencia && m.conta == t.conta) {
-                    existe = true;
-                    if (t.agenciaDestino != 0) {
-                        m.movimentacaoEletronica += t.transacao;
-                    } else {
-                        m.movimentacaoEspecie += t.transacao;
-                    }
-                    m.qtdTransacoes++;
-                    break;
-                }
-            }
-            if (!existe) {
-                c.agencia = t.agencia;
-                c.conta = t.conta;
-                if (t.agenciaDestino != 0) {
-                    c.movimentacaoEletronica = t.transacao;
-                    c.movimentacaoEspecie = 0;
-                } else {
-                    c.movimentacaoEspecie = t.transacao;
-                    c.movimentacaoEletronica = 0;
-                }
-                c.qtdTransacoes = 1;
-                movimentacoesFiltradas.push_back(c);
-            }
-        }
-    }
-    return movimentacoesFiltradas;
-}
-
-vector<Transacao> lerTransacoes(const string& nomeArquivo) {
+vector<Transacao> lerArquivoTransacoes(const string& nomeArquivo) {
     ifstream arquivo(nomeArquivo);
     vector<Transacao> transacoes;
 
@@ -169,11 +132,92 @@ vector<Transacao> lerTransacoes(const string& nomeArquivo) {
     return transacoes;
 }
 
+vector<Cliente> filtrarMovimentacoesPorMesAno(vector<Transacao>& transacoes, short mesDesejado, short anoDesejado) {
+    vector<Cliente> movimentacoesFiltradas;
+
+    for (const auto& t : transacoes) {
+        if (t.mes == mesDesejado && t.ano == anoDesejado) {
+            Cliente c;
+            bool existe = false;
+            for (auto& m : movimentacoesFiltradas) {
+                if (m.agencia == t.agencia && m.conta == t.conta) {
+                    existe = true;
+                    if (t.agenciaDestino != 0) {
+                        m.movimentacaoEletronica += t.transacao;
+                    } else {
+                        m.movimentacaoEspecie += t.transacao;
+                    }
+                    m.qtdTransacoes++;
+                    break;
+                }
+            }
+            if (!existe) {
+                c.agencia = t.agencia;
+                c.conta = t.conta;
+                if (t.agenciaDestino != 0) {
+                    c.movimentacaoEletronica = t.transacao;
+                    c.movimentacaoEspecie = 0;
+                } else {
+                    c.movimentacaoEspecie = t.transacao;
+                    c.movimentacaoEletronica = 0;
+                }
+                c.qtdTransacoes = 1;
+                movimentacoesFiltradas.push_back(c);
+            }
+        }
+    }
+    return movimentacoesFiltradas;
+}
+
+void salvarConsolidadas(const string& nomeArquivo, vector<Cliente>& movimentacoes) {
+    ofstream arquivo(nomeArquivo, ios::binary);
+
+    if (!arquivo.is_open()) {
+        cerr << "ERROR: Erro ao abrir o arquivo!" << endl;
+    }
+
+    for (const auto& m : movimentacoes) {
+        arquivo.write((char*)&m, sizeof(Cliente));
+    }
+
+    arquivo.close();
+}
+
+vector<Cliente> carregarConsolidadas(const string& nomeArquivo) {
+    ifstream arquivo(nomeArquivo);
+    vector<Cliente> movimentacoes;
+
+    if (!arquivo.is_open()) {
+        cerr << "ERROR: Erro ao abrir o arquivo!" << endl;
+        return movimentacoes;
+    }
+
+    Cliente c;
+    while (arquivo.read((char*)&c, sizeof(Cliente))) {
+        movimentacoes.push_back(c);
+    }
+
+    arquivo.close();
+    return movimentacoes;
+}
+
+void atualizaLog(string mensagem) {
+    // pega a data e o horário atual
+    time_t agora = time(0);
+    tm *ltm = localtime(&agora);
+    string tempoLocal = to_string(ltm->tm_mday) + "/" + to_string(1 + ltm->tm_mon) + "/" + to_string(1900 + ltm->tm_year) + " " + to_string(ltm->tm_hour) + ":" + to_string(ltm->tm_min) + ":" + to_string(ltm->tm_sec);
+
+    ofstream log ("log.txt", std::ios::app);
+    log << tempoLocal << " - " << mensagem << endl;
+    log.close();
+}
+
 int menu() {
     int opcao;
     cout << endl;
     cout << "Menu" << endl;
-    cout << "1 - Filtrar por mês e ano" << endl;
+    cout << "1 - Consultar por mês e ano" << endl;
+    cout << "2 - Filtrar consulta por valor" << endl;
     cout << "0 - Sair" << endl;
     cout << "Digite a opção desejada: ";
     cin >> opcao;
@@ -186,17 +230,17 @@ int main() {
     cout << "Bem-vindo ao sistema de auditoria de transações!" << endl << endl;
 
 
+    int opcaoArquivo = 2;
     cout << "Qual arquivo deseja ler?" << endl;
     cout << "1 - 1k transações" << endl;
     cout << "2 - 10k transações" << endl;
     cout << "3 - Todas as transações" << endl;
     cout << "Digite a opção desejada: ";
-    int opcaoArquivo;
     cin >> opcaoArquivo;
 
 
     cout << "Lendo arquivo de transações..." << endl;
-    vector<Transacao> transacoes = lerTransacoes(opcaoArquivo == 1 ? FILE_NAME_1 : opcaoArquivo == 2 ? FILE_NAME_10 : FILE_NAME_F);
+    vector<Transacao> transacoes = lerArquivoTransacoes(opcaoArquivo == 1 ? FILE_NAME_1 : opcaoArquivo == 2 ? FILE_NAME_10 : FILE_NAME_F);
     cout << "Arquivo lido com sucesso!" << endl;
 
 
@@ -215,52 +259,32 @@ int main() {
             cout << "Digite o mês e o ano desejado (ex: 1 2019):";
             cin >> mes >> ano;
 
-            string logDados = to_string(mes) + to_string(ano);
+            string mesAno = to_string(mes) + to_string(ano);
             string line;
-            bool existe = false;
+
+            string nomeDados;
+            nomeDados = mes < 10 ? "0" + mesAno : mesAno;
 
             clearScreen();
             cout << "Filtrando por: " << mes << "/" << ano << endl;
             
-            ifstream arquivoLog ("log.txt");
-            if (arquivoLog.is_open()) {
-                while ( getline(arquivoLog, line)) {
-                    if (logDados == line) {
-                        existe = true;
-                        cout << "Já existe um log para esse mês e ano" << endl;
-                        break;
-                    }
+            ifstream file("consolidadas" + nomeDados + ".bin");
+
+            if (file) {
+                file.close();
+                movimentacoes = carregarConsolidadas("consolidadas" + nomeDados + ".bin");
+
+                Cliente c;
+                cout << "agencia - conta - especie - eletronica - total - qtdTransacoes" << endl;
+                for (const auto& c : movimentacoes) {
+                    cout << c.agencia << " " << c.conta << " "
+                        << c.movimentacaoEspecie << " " << c.movimentacaoEletronica 
+                        << " " << c.qtdTransacoes << endl;
                 }
-                arquivoLog.close();
-            }
 
-            string nomeDados;
-            if (mes < 10) {
-                nomeDados = "0" + to_string(mes) + to_string(ano);
             } else {
-                nomeDados = to_string(mes) + to_string(ano);
-            }
-
-            if (existe) {
-                ifstream dadosBin ("consolidadas" + nomeDados + ".bin", ios::binary);
-
-                if (dadosBin.is_open()) {
-                    Cliente c;
-                    while (dadosBin.read((char*)&c, sizeof(Cliente))) {
-                        cout << c.agencia << " " << c.conta << " "
-                            << c.movimentacaoEspecie << " " << c.movimentacaoEletronica 
-                            << " " << c.qtdTransacoes << endl;
-                    }
-                    dadosBin.close();
-                } else {
-                    cout << "Erro ao abrir arquivo de dados!" << endl;
-                }
-            } else {
-                ofstream arquivoLog ("log.txt", std::ios::app);
-                arquivoLog << logDados << endl;
-                arquivoLog.close();
-
                 movimentacoes = filtrarMovimentacoesPorMesAno(transacoes, mes, ano);
+                salvarConsolidadas("consolidadas" + nomeDados + ".bin", movimentacoes);
 
                 cout << "agencia - conta - especie - eletronica - total - qtdTransacoes" << endl;
                 for (const auto& m : movimentacoes) {
@@ -268,16 +292,70 @@ int main() {
                         << m.movimentacaoEspecie << " " << m.movimentacaoEletronica 
                         << " " << m.qtdTransacoes << endl;
                 }
-
-                ofstream dadosBin ("consolidadas" + nomeDados + ".bin", ios::binary);
-                for (const auto& m : movimentacoes) {
-                    dadosBin.write((char*)&m, sizeof(Cliente));
-                }
-                dadosBin.close();
             }
+        } else if (opcao == 2) {
+            float valorMinimoEspecie;
+            float valorMinimoEletronica;
+            short tipoFiltro;
+            short mes, ano;
+            int qtdOperacoes = 0;
+
+            cout << "Digite o mês e o ano desejado (ex: 1 2019):";
+            cin >> mes >> ano;
+
+            cout << "Insira o valor mínimo de movimentação em espécie" << endl;
+            cin >> valorMinimoEspecie;
+
+            cout <<  "Insira o valor mínimo de movimentação eletrônica" << endl;
+            cin >> valorMinimoEletronica;
+
+            cout << "Escolha o tipo de filtro" << endl;
+            cout << "1 - x OU y" << endl;
+            cout << "2 - x E y" << endl;
+            cin >> tipoFiltro;
+
+            clearScreen();
+
+            cout << "Filtrando a partir de : " << valorMinimoEspecie << " em espécie " << (tipoFiltro == 1 ? "OU " : "E ") << valorMinimoEletronica << " eletrônica" << endl;
+
+            string nomeDados = (mes < 10 ? "0"  : "mesAno") + to_string(mes) + to_string(ano);;
+
+            ifstream file("consolidadas" + nomeDados + ".bin");
+            if (file) {
+                file.close();
+                movimentacoes = carregarConsolidadas("consolidadas" + nomeDados + ".bin");
+            } else {
+                movimentacoes = filtrarMovimentacoesPorMesAno(transacoes, mes, ano);
+                salvarConsolidadas("consolidadas" + nomeDados + ".bin", movimentacoes);
+            }
+
+            cout << "agencia - conta - especie - eletronica - total - qtdTransacoes" << endl;
+            for (const auto& m : movimentacoes) {
+                if (tipoFiltro == 1) {
+                    if (abs(m.movimentacaoEspecie) >= valorMinimoEspecie || abs(m.movimentacaoEletronica) >= valorMinimoEletronica) {
+                        cout << m.agencia << " " << m.conta << " "
+                            << m.movimentacaoEspecie << " " << m.movimentacaoEletronica 
+                            << " " << m.qtdTransacoes << endl;
+                        qtdOperacoes++;
+                    }
+                } else if (tipoFiltro == 2) {
+                    if (abs(m.movimentacaoEspecie) >= valorMinimoEspecie && abs(m.movimentacaoEletronica) >= valorMinimoEletronica) {
+                        cout << m.agencia << " " << m.conta << " "
+                            << m.movimentacaoEspecie << " " << m.movimentacaoEletronica 
+                            << " " << m.qtdTransacoes << endl;
+                        qtdOperacoes++;
+                    }
+                }
+            }
+
+            atualizaLog(to_string(mes) + "/" + to_string(ano) + " - " + to_string(valorMinimoEspecie) + " - " + to_string(valorMinimoEletronica) + " - " + (tipoFiltro == 1 ? "OU" : "E") + " - " +to_string(qtdOperacoes));
+
+        } else {
+            cout << "Opção inválida!" << endl;
         }
     }
 
+    clearScreen();
     cout << "Programa Finalizado!" << endl;
     return 0;
 }
