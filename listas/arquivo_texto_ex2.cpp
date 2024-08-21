2cae6fd75498085eab7c341ce6fd7bd069b56e03
Emanuel Bravo <emanuelbravo0706@gmail.com>
2024-08-21 10:11:28 -0300

    Update arquivo_texto_ex2.cpp

--- listas/arquivo_texto_ex2.cpp
+++ listas/arquivo_texto_ex2.cpp
@@ -6,14 +6,6 @@
 #include <string>
 #include <algorithm>
 
-
-//  2. Faça um programa que leia o arquivo texto “valores.txt” que está no formato CSV (commaseparated values). Nesse formato, cada linha possui diversos valores (inteiros, reais ou strings) que
-// estão separadas uns dos outros apenas por uma vírgula (não há espaços em brancos entre os
-// valores; espaços em branco só existem entre duas palavras da mesma string). O programa deve
-// mostrar quantos números inteiros, reais e strings existem em cada linha do arquivo. Um número
-// só será considerando como real se possuir “.” (ex: 5.478). Um número só será considerado inteiro
-// se possuir apenas algarismos de “0..9”. Qualquer outro tipo de informação é considerado string.
-
 using namespace std;
 
 bool isInt(string& cell) {
@@ -23,13 +15,6 @@ bool isInt(string& cell) {
         return false;
     }
 }
-
-// bool isInt(const std::string& s)
-// {
-//     std::string::const_iterator it = s.begin();
-//     while (it != s.end() && std::isdigit(*it)) ++it;
-//     return !s.empty() && it == s.end();
-// }
 
 bool isFloat(string& cell) {
     istringstream iss(cell);
@@ -66,13 +51,10 @@ int main() {
             for (int i = 0; i < line_vector.size(); i++) {
                 string temp = line_vector[i];
                 if (isInt(temp)) {
-                    // cout << temp << " is int" << endl;
                     n_int++;
                 } else if (isFloat(temp)) {
-                    // cout << temp << " is float" << endl;
                     n_float++;
                 } else {
-                    // cout << temp << " is string" << endl;
                     n_string++;
                 }
             }
